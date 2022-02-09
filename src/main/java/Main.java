0c93aad4e50503573fc0d0efbc058ea8c6ffd06c
lmoneta <lorenzo.moneta@techedgegroup.com>
2022-02-09 17:51:01 +0100

    Read list of map termsAndConditions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,11 +15,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         final String tableName = "elg-dev-dynamostack-elgdevdynamostackusers106BDBCA-FVS3ZG6Q8VLR";
-        String contactId = "0031X00000n323sQAA";
+        String contactId = "0031X00000moLOlQAM";
         if (args.length > 0) {
             contactId = args[0];
         }
@@ -47,5 +49,14 @@ public class Main {
         JsonParser jp = new JsonParser();
         String json = gson.toJson(jp.parse(outcome.toString()));
         System.out.println(json);
+        List<Map<String, Object>> ma = outcome.getList("termsAndConditions");
+        System.out.println("\n_______________________\nTerms and conditions:");
+        for(Map<String, Object> m : ma) {
+            for (String name : m.keySet()) {
+                String key = name;
+                String value = m.get(name).toString();
+                System.out.println(key + " " + value);
+            }
+        }
     }
 }
