e4b5e9b50efcf14d9c0435c4b0e6dd1d7aefe046
lmoneta <lorenzo.moneta@techedgegroup.com>
2022-02-09 16:10:53 +0100

    Parametrized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.profile.ProfilesConfigFile;
+import com.amazonaws.internal.StaticCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -14,32 +12,32 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final String roleArn = "arn:aws:iam::789329792194:role/group-tech-dev";
-        final String roleSessionName = "group-tech-dev";
         final String tableName = "elg-dev-dynamostack-elgdevdynamostackusers106BDBCA-FVS3ZG6Q8VLR";
         String contactId = "0031X00000n323sQAA";
-        if(args.length>0){
-            contactId=args[0];
+        if (args.length > 0) {
+            contactId = args[0];
         }
-        System.out.print("Insert aws profile name: ");
+        System.out.print("Insert aws profile name [default]: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String profile = null;
         try {
             profile = br.readLine();
+            if (profile.equals("")) {
+                profile = "default";
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        AWSCredentialsProvider provider = new ProfileCredentialsProvider(System.getProperty("user.home") + "/.aws/config", profile);
-        STSAssumeRoleSessionCredentialsProvider sar = new STSAssumeRoleSessionCredentialsProvider(provider, roleArn, roleSessionName);
-        AWSCredentials credentials = provider.getCredentials();
+        ProfilesConfigFile pcf = new ProfilesConfigFile(new File(System.getProperty("user.home") + "/.aws/config"));
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.EU_WEST_1)
-                .withCredentials(sar)
+                .withCredentials(new StaticCredentialsProvider(pcf.getCredentials(profile)))
                 .build();
         DynamoDB dynamoDB = new DynamoDB(client);
         Table table = dynamoDB.getTable(tableName);
@@ -48,7 +46,6 @@ public class Main {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonParser jp = new JsonParser();
         String json = gson.toJson(jp.parse(outcome.toString()));
-        ;
         System.out.println(json);
     }
 }
